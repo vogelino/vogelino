7e6010c75d0d341b1b28539c55a68a6e913fa9c9
Lucas Vogel <contact@vogelino.com>
2022-09-19 23:56:01 +0200

    Add script and astro image tools to have optimized images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroImageTools],
 });
