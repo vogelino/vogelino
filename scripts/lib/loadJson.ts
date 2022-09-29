7c0ffce270d894ecfffcc882dc00c702738dd084
Lucas Vogel <contact@vogelino.com>
2022-09-30 00:51:16 +0200

    Use cache data in pages

---
+++ scripts/lib/loadJson.ts
@@ -0,0 +1,9 @@
+import { readFile } from "node:fs/promises";
+
+export async function loadJson<
+  T = Record<string, unknown> | Record<string, unknown>[]
+>(pathToFile: string): Promise<T> {
+  const file = await readFile(pathToFile, "utf-8");
+  const json = await JSON.parse(file);
+  return json as T;
+}
