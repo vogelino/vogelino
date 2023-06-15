59e3aed136b5b88253599213683e22e0079eeccd
Lucas Vogel <contact@vogelino.com>
2023-06-14 22:14:09 -0400

    refactor(Thumbnail): Show directly screenshot on touch devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,7 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/container-queries')
+    require('@tailwindcss/container-queries'),
+    require('tailwindcss-touch')(),
   ],
 };
