89cc08632d6c3912858f8f04f6f7335fed7efb98
Lucas Vogel <contact@vogelino.com>
2023-06-28 12:39:35 -0400

    refactor: Add vercel config to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+import sitemap from '@astrojs/sitemap';
 import { astroImageTools } from "astro-imagetools";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools],
+  integrations: [tailwind(), prefetch(), sitemap(), astroImageTools],
+  adapter: vercel({
+    analytics: true
+  }),
 });
