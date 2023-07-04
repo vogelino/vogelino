68c16347f398a3db567c9b713faa0403b4bc7b43
Lucas Vogel <contact@vogelino.com>
2023-07-04 19:38:23 -0400

    chore(inspirations): Update inspirations

--- release.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: "@technologiestiftung/semantic-release-config",
-};
---
+++ src/colors.js
@@ -0,0 +1,11 @@
+export default {
+  fg: "#e30002",
+  fgDark: "#a40002",
+  bg: "#FCFEFF",
+  alt: "#ffef9b",
+  grayDark: "#5D7280",
+  grayMed: "#D0DFE7",
+  grayLight: "#E1ECF2",
+  grayUltraLight: "#F1FAFF",
+}
+
