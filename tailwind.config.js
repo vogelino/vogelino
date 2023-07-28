e34d868cc4c5864846fe73240b7b0354bad62ebb
Lucas Vogel <contact@vogelino.com>
2023-07-28 17:27:17 +0200

    style(LogoInteractive): Show small logo on mobile scroll start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,67 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
+const plugin = require('tailwindcss/plugin')
 
 const fallbackFonts = [
-  "ui-sans-serif", "system-ui",
-  "-apple-system",
-  "BlinkMacSystemFont",
-  "Segoe UI",
-  "Roboto",
-  "Helvetica Neue",
-  "Arial",
-  "Noto Sans",
-  "sans-serif",
-  "Apple Color Emoji",
-  "Segoe UI Emoji",
-  "Segoe UI Symbol",
-  "Noto Color Emoji",
-];
+	'ui-sans-serif',
+	'system-ui',
+	'-apple-system',
+	'BlinkMacSystemFont',
+	'Segoe UI',
+	'Roboto',
+	'Helvetica Neue',
+	'Arial',
+	'Noto Sans',
+	'sans-serif',
+	'Apple Color Emoji',
+	'Segoe UI Emoji',
+	'Segoe UI Symbol',
+	'Noto Color Emoji',
+]
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./pages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        special: ["lobular", ...fallbackFonts],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./pages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				special: ['lobular', ...fallbackFonts],
 
-        sans: ["FUNGIS", ...fallbackFonts],
-        serif: ["serif"],
-        mono: ["monospace"],
-      },
-      colors: {
-        fg: "#e30002",
-        fgDark: "#a40002",
-        bg: "#FCFEFF",
-        alt: "#ffef9b",
-        grayDark: "#5D7280",
-        grayMed: "#D0DFE7",
-        grayLight: "#E1ECF2",
-        grayUltraLight: "#F1FAFF",
-      },
-      backgroundImage: {
-        "tooth-pattern": "url('/images/tooth-pattern.svg')",
-      },
-      height: { screen: '100svh' },
-      width: { screen: '100svw' },
-      screens: {
-        xs: '420px',
-      }
-    },
-  },
-  plugins: [
-    require('@tailwindcss/container-queries'),
-    require('tailwindcss-touch')(),
-    plugin(function({ addUtilities }) {
-      addUtilities({
-        '.text-balance': {
-          'text-wrap': 'balance',
-        }
-      })
-    })
-  ],
-};
+				sans: ['FUNGIS', ...fallbackFonts],
+				serif: ['serif'],
+				mono: ['monospace'],
+			},
+			colors: {
+				fg: '#e30002',
+				fgDark: '#a40002',
+				bg: '#FCFEFF',
+				alt: '#ffef9b',
+				grayDark: '#5D7280',
+				grayMed: '#D0DFE7',
+				grayLight: '#E1ECF2',
+				grayUltraLight: '#F1FAFF',
+			},
+			backgroundImage: {
+				'tooth-pattern': "url('/images/tooth-pattern.svg')",
+			},
+			height: { screen: '100lvh' },
+			width: { screen: '100lvw' },
+			screens: {
+				xs: '420px',
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/container-queries'),
+		require('tailwindcss-touch')(),
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+				'.text-balance': {
+					'text-wrap': 'balance',
+				},
+			})
+		}),
+	],
+}
