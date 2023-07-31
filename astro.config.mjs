8fe35b01a43a6ec747a3a64e71793dd22650f8ae
Lucas Vogel <contact@vogelino.com>
2023-07-31 11:53:35 +0200

    fix(sitemap): Add url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
-import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import prefetch from '@astrojs/prefetch'
+import sitemap from '@astrojs/sitemap'
+import { astroImageTools } from 'astro-imagetools'
 
 /**
  * @type {import('astro-imagetools').GlobalConfigOptions}
  */
 export default defineConfig({
-  integrations: [tailwind(), prefetch(), sitemap(), astroImageTools],
-});
+	integrations: [tailwind(), prefetch(), sitemap(), astroImageTools],
+	site:
+		process.env.BASE_URL || process.env.VERCEL_URL || `https://vogelino.com`,
+})
