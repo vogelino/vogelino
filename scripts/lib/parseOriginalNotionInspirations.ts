00fa7ec4da6f4033af94288baebe947ad278c612
Lucas Vogel <contact@vogelino.com>
2024-01-15 22:24:44 -0300

    refactor(inspirations): Validate inspirations using zod

--- scripts/lib/parseOriginalNotionInspirations.ts
+++ scripts/lib/parseOriginalNotionInspirations.ts
@@ -1,37 +1,46 @@
-import type { RawNotionInspirationLinkType } from "./getOriginalNotionInspirations";
+import { inspirationSchema } from '../../src/schemas/inspirations'
+import type { RawNotionInspirationLinkType } from './getOriginalNotionInspirations'
 
 export interface MappedNotionInspirationLinkType
-  extends Record<string, unknown> {
-  id: string;
-  title: string;
-  url: string;
-  thumbnail: string | undefined;
-  icon: string | { emoji: string };
+	extends Record<string, unknown> {
+	id: string
+	title: string
+	url: string
+	thumbnail: string | undefined
+	icon: string | { emoji: string }
 }
 
 function mapNotionInspirationLink(
-  rawLink: RawNotionInspirationLinkType
+	rawLink: RawNotionInspirationLinkType,
 ): MappedNotionInspirationLinkType {
-  const { icon, cover } = rawLink;
-  const { Name, URL } = rawLink.properties;
-  let iconToReturn: string | { emoji: string } = "";
-  if (icon) {
-    if ("emoji" in icon) iconToReturn = icon;
-    else {
-      iconToReturn = (icon?.file || icon?.external)?.url || "";
-    }
-  }
-  return {
-    id: rawLink.id,
-    title: Name.title.map((item) => item.plain_text).join(""),
-    url: URL.url,
-    thumbnail: `/images/inspirations/${rawLink.id}.webp`,
-    icon: iconToReturn,
-  };
+	const { icon } = rawLink
+	const { Name, URL } = rawLink.properties
+	let iconToReturn: string | { emoji: string } = ''
+	if (icon) {
+		if ('emoji' in icon) iconToReturn = icon
+		else {
+			iconToReturn = (icon?.file || icon?.external)?.url || ''
+		}
+	}
+	const inspiration = {
+		id: rawLink.id,
+		title: Name.title.map((item) => item.plain_text).join(''),
+		url: URL.url,
+		thumbnail: `/images/inspirations/${rawLink.id}.webp`,
+		icon: iconToReturn,
+	}
+	const validation = inspirationSchema.safeParse(inspiration)
+	if (!validation.success) {
+		console.log(
+			`Error parsing inspiration ${inspiration.id}: ${validation.error}`,
+			JSON.stringify(inspiration, null, 2),
+		)
+	}
+	return inspiration
 }
 
 export async function parseOriginalNotionInspirations(
-  originalInspirations: RawNotionInspirationLinkType[]
+	originalInspirations: RawNotionInspirationLinkType[],
 ): Promise<MappedNotionInspirationLinkType[]> {
-  return originalInspirations.map(mapNotionInspirationLink);
+	return originalInspirations.map(mapNotionInspirationLink)
 }
