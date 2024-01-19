fe475b9b3ddeb4add532c3d0a1589f3c3660563e
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-19 14:09:41 +0530

    use variable and remove orrdeide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin')
 
+const fallbackFonts = [
+	'ui-sans-serif',
+	'system-ui',
+	'-apple-system',
+	'BlinkMacSystemFont',
+	'Segoe UI',
+	'Roboto',
+	'Helvetica Neue',
+	'Arial',
+	'Noto Sans',
+	'sans-serif',
+	'Apple Color Emoji',
+	'Segoe UI Emoji',
+	'Segoe UI Symbol',
+	'Noto Color Emoji',
+]
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ['class', '[data-applied-mode="dark"]'],
