b8082fd190c335876a6f081783d5e9d739621015
Lucas Vogel <contact@vogelino.com>
2024-04-04 23:01:45 -0300

    ci(critters): Add critters to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import critters from '@playform/inline'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-	integrations: [tailwind(), mdx(), sitemap()],
+	integrations: [tailwind(), mdx(), sitemap(), critters()],
 	prefetch: true,
 	site: `https://vogelino.com`,
 })
