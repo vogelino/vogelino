62bb9157dfd8040ba34ce0a4a4cd57e83bbee45c
Lucas Vogel <contact@vogelino.com>
2024-04-19 11:57:54 -0400

    fix: Fix prefix of env var in deploy function

--- api/deploy.js
+++ api/deploy.js
@@ -1,7 +1,8 @@
 const fetch = require("node-fetch");
 
 module.exports = async (_req, res) => {
-  if (!VERCEL_DEPLOY_HOOK_URL) return res.status(200).json({ success: true });
+  if (!process.env.VERCEL_DEPLOY_HOOK_URL)
+    return res.status(200).json({ success: true });
   const response = await fetch(process.env.VERCEL_DEPLOY_HOOK_URL, {
     method: "POST",
     headers: {
