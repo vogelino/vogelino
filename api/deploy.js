9c36165172ff3af45be43e7425acff8a13a0cccd
Lucas Vogel <contact@vogelino.com>
2024-04-19 12:09:51 -0400

    ci: Make deploy hook need to pass authorization token and url

--- api/deploy.js
+++ api/deploy.js
@@ -1,21 +1,57 @@
 const fetch = require("node-fetch");
+const zod = require("zod");
 
-module.exports = async (_req, res) => {
-  if (!process.env.VERCEL_DEPLOY_HOOK_URL)
-    return res.status(200).json({ success: true });
-  const response = await fetch(process.env.VERCEL_DEPLOY_HOOK_URL, {
+const requestBodySchema = zod.object({
+  webhookUrl: zod.string().url(),
+});
+
+module.exports = async (req, res) => {
+  let reqBody;
+  try {
+    reqBody = await req.json();
+  } catch (error) {
+    const statusCode = error.status || 400;
+    return res.status(statusCode).json({
+      success: false,
+      error: `Invalid request body: ${error.message}`,
+    });
+  }
+  try {
+    reqBody = requestBodySchema.parse(reqBody);
+  } catch (error) {
+    const statusCode = error.status || 400;
+    return res.status(statusCode).json({
+      success: false,
+      error: `Invalid request body: ${error.message}`,
+    });
+  }
+  const authBearerToken = req.headers.authorization;
+  if (!authBearerToken) {
+    return res.status(401).json({
+      success: false,
+      error: "Unauthorized",
+    });
+  }
+  const response = await fetch(reqBody.webhookUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
+      Authorization: `Bearer ${authBearerToken}`,
     },
-    body: JSON.stringify({
-      env: {
-        BUILD_TYPE: "webhook",
-      },
-    }),
+    body: JSON.stringify({ env: { BUILD_TYPE: "webhook" } }),
   });
 
-  const result = await response.json();
-  res.status(200).json({ success: true, result });
+  if (!response.ok) {
+    return res.status(response.statusCode).json({
+      success: false,
+      error: `Failed to deploy: ${response.statusText}`,
+    });
+  }
+
+  try {
+    const result = await response.json();
+    res.status(200).json({ success: true, result });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
 };
