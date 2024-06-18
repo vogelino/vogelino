af3300665e6aa48fc8ee623f97ef3d69c66b0fa6
Lucas Vogel <contact@vogelino.com>
2024-06-17 22:22:01 -0400

    feat(Sentry): Add Sentry custom events

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,21 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import critters from '@playform/inline'
-import sentry from '@sentry/astro'
-import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import solidJs from '@astrojs/solid-js';
+import tailwind from '@astrojs/tailwind';
+import critters from '@playform/inline';
+import sentry from '@sentry/astro';
+import { defineConfig } from 'astro/config';
 
-import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		mdx(),
-		sitemap(),
-		critters(),
-		sentry({
-			dsn: process.env.SENTRY_DSN,
-			sourceMapsUploadOptions: {
-				project: 'vogelino-portfolio',
-				authToken: process.env.SENTRY_AUTH_TOKEN,
-			},
-		}),
-		solidJs(),
-	],
-	prefetch: true,
-	site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://vogelino.com',
-})
+  integrations: [tailwind(), mdx(), sitemap(), critters(), sentry({
+    dsn: process.env.SENTRY_DSN,
+    sourceMapsUploadOptions: {
+      project: 'vogelino-portfolio',
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    }
+  }), solidJs()],
+  prefetch: true,
+  site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://vogelino.com'
+});
