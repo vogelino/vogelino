16949bd9fcf4d8bccf34f1d54784dac4c8451cec
Lucas Vogel <contact@vogelino.com>
2024-08-02 16:42:10 -0400

    feat(inspirations): Fix inspirations

--- src/utils/getParsedInspirations.ts
+++ src/utils/getParsedInspirations.ts
@@ -1,3 +1,4 @@
+import { ZodError } from 'zod'
 import { loadJson } from '../../scripts/lib/loadJson'
 import { INSPIRATIONS_JSON_PATH } from '../../scripts/paths'
 import { type InspirationType, inspirationSchema } from '../schemas/inspirations'
@@ -7,15 +8,36 @@ export async function getParsedInspirations(): Promise<InspirationType[]> {
 	const rawInspirations = await loadJson<InspirationType[]>(INSPIRATIONS_JSON_PATH)
 	const parsedInspirations = rawInspirations.reduce((acc, inspiration) => {
 		const { thumbnail, favicon } = getInspirationImages(inspiration.id)
+		const newInspiration = { ...inspiration, thumbnail, favicon }
 		try {
-			acc.push(inspirationSchema.parse({ ...inspiration, thumbnail, favicon }))
+			const newParsedInspiration = inspirationSchema.parse(newInspiration)
+			acc.push(newParsedInspiration)
 			return acc
 		} catch (error) {
-			console.log('Error parsing inspiration:', JSON.stringify(inspiration, null, 2))
-			console.log(error)
+			if (error instanceof ZodError) {
+				logZodErrorToTerminal(error, newInspiration)
+			} else {
+				console.log(error)
+			}
 			return acc
 		}
 	}, [] as InspirationType[])
 
 	return parsedInspirations
+}
+
+function logZodErrorToTerminal(error: ZodError, newInspiration: Record<string, unknown>) {
+	const name =
+		typeof newInspiration === 'object' &&
+		newInspiration !== null &&
+		('title' in newInspiration || 'id' in newInspiration)
+			? newInspiration.title || newInspiration.id
+			: 'Unknown Inspiration'
+	console.log('\n\n––––––––––––––––––––––––––')
+	console.log(`Cool Site Parsing Error for "${name}":`)
+	for (const issue of error.issues) {
+		const formattedIssueMessage = ` - ${issue.path.join('.')}: ${issue.message}`
+		console.log(formattedIssueMessage)
+	}
+	console.log('––––––––––––––––––––––––––\n\n')
 }
