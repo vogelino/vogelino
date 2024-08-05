63b4010f21483ec8eec4428d9f47ab8901e49d38
Lucas Vogel <contact@vogelino.com>
2024-08-04 21:12:20 -0400

    refactor(parseOriginalNotionInspirations): Remove duplicate ids

--- src/schemas/inspirations.ts
+++ src/schemas/inspirations.ts
@@ -1,22 +1,22 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 const imageSchema = z
-	.object({
-		src: z.string(),
-		width: z.number(),
-		height: z.number(),
-		format: z.enum(['jpg', 'jpeg', 'png', 'webp', 'avif', 'svg']),
-		orientation: z.number().optional(),
-	})
-	.optional()
+  .object({
+    src: z.string(),
+    width: z.number(),
+    height: z.number(),
+    format: z.enum(["jpg", "jpeg", "png", "webp", "avif", "svg"]),
+    orientation: z.number().optional(),
+  })
+  .optional();
 
 export const inspirationSchema = z.object({
-	id: z.string(),
-	title: z.string(),
-	url: z.string().url(),
-	thumbnail: imageSchema,
-	favicon: imageSchema,
-	date: z.string(),
-	tags: z.string().array().default([]),
-})
-export type InspirationType = z.infer<typeof inspirationSchema>
+  id: z.string(),
+  title: z.string(),
+  url: z.string().url(),
+  thumbnail: imageSchema,
+  favicon: imageSchema,
+  date: z.string(),
+  tags: z.string().array().default([]),
+});
+export type InspirationType = z.infer<typeof inspirationSchema>;
