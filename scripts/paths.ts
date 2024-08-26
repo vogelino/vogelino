6806fe61800cf636d38c14bde2623493fa9f0b98
Lucas Vogel <contact@vogelino.com>
2024-08-26 17:41:07 -0400

    update paths to be relative (test compat vercel)

--- scripts/paths.ts
+++ scripts/paths.ts
@@ -1,6 +1,6 @@
-export const IMAGE_TMP_EXPORT_PATH = 'data/images/tmp'
-export const IMAGE_DEST_EXPORT_PATH = 'src/assets/images'
-export const COOL_SITE_RESIZED_EXPORT_PATH = `${IMAGE_DEST_EXPORT_PATH}/cool-sites`
-export const COOL_SITE_FAVICON_EXPORT_PATH = `${IMAGE_DEST_EXPORT_PATH}/cool-sites-favicons`
-export const COOL_SITES_JSON_PATH = 'src/assets/data/cool-sites.json'
-export const MEDIA_SHELF_JSON_PATH = 'src/assets/data/media-shelf.json'
+export const IMAGE_TMP_EXPORT_PATH = './data/images/tmp'
+export const IMAGE_DEST_EXPORT_PATH = './src/assets/images'
+export const COOL_SITE_RESIZED_EXPORT_PATH = `./${IMAGE_DEST_EXPORT_PATH}/cool-sites`
+export const COOL_SITE_FAVICON_EXPORT_PATH = `./${IMAGE_DEST_EXPORT_PATH}/cool-sites-favicons`
+export const COOL_SITES_JSON_PATH = './src/assets/data/cool-sites.json'
+export const MEDIA_SHELF_JSON_PATH = './src/assets/data/media-shelf.json'
